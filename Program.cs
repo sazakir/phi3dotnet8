9e629162e3745f28a94a87967ad30812e5043626
Shakeel <shakeel.ahmed@intelizign.com>
2024-10-07 18:33:18 +0530

    code refactoring done

--- Program.cs
+++ Program.cs
@@ -4,16 +4,16 @@ using System.Text.Json;
 
 var app = WebApplication.Create(args);
 
+app.MapGet("/health", () => "200 OK. Running!");
+
 var homePage = File.ReadAllText("index.htm");
 app.MapGet("/", () => Results.Content(homePage, "text/html"));
 
 var ico = File.ReadAllBytes("favicon.ico");
 app.MapGet("/favicon.ico", GetFavIcon);
 
-app.MapGet("/health", () => "200-OK:: Running!");
-
 var handler = new Phi3Chat();
-app.MapGet("/chat", handler.Chat);
+app.MapGet("/chat", handler.GetModelResponse);
 
 app.Run();
 
@@ -26,38 +26,27 @@ async Task GetFavIcon(HttpContext context)
 
 public class Phi3Chat
 {
-    private string sessionLogFile = string.Empty;
-
-    private void InitializeLogFile()
-    {
-        sessionLogFile = Path.Combine(Path.GetTempPath(), "log_" + Guid.NewGuid().ToString() + DateTime.Now.ToLongDateString() + ".log.txt");
-    }
+    private string logFile = string.Empty;
 
     private void Log(string message)
     {
         try
         {
-            File.AppendAllText(sessionLogFile, DateTime.Now.ToShortTimeString() + "\t" + message + Environment.NewLine);
+            File.AppendAllText(logFile, DateTime.Now.ToShortTimeString() + "\t" + message + Environment.NewLine);
         }
         catch { };
     }
 
-    public IResult Chat(string q)
+    private object GetQueryObject(string userQuery)
     {
-        InitializeLogFile();
-        Log("Initializing chat for query: " + q);
 
-        dynamic? json;
-
-        try
+        var query = new
         {
-            var query = new
-            {
-                model = AppConstants.OLLAMA_MODEL,
-                stream = AppConstants.STREAM_RESPONSE,
-                max_tokens = AppConstants.OLLAMA_API_RESPONSE_MAX_TOKENS,
+            model = AppConstants.OLLAMA_MODEL,
+            stream = AppConstants.STREAM_RESPONSE,
+            max_tokens = AppConstants.OLLAMA_API_RESPONSE_MAX_TOKENS,
 
-                messages = new object[]
+            messages = new object[]
                 {
                     new {
                         role = "system",
@@ -65,39 +54,71 @@ public class Phi3Chat
                         },
                     new {
                         role = "user",
-                        content = q
+                        content = userQuery
                         }
                 }
-            };
+        };
 
-            var stringObj = JsonSerializer.Serialize(query);
-            Log("Query object:");
+        return query;
+
+    }
+
+    public IResult GetModelResponse(string userQuery)
+    {
+        try
+        {
+            //initialize a simple text log for the current session in the temp folder
+            logFile = Path.Combine(Path.GetTempPath(),
+                                            "log_" + Guid.NewGuid().ToString() +
+                                                    DateTime.Now.ToString("yyyy_MM_dd_HH_mm")
+                                                    + ".log.txt");
+
+            Log("Initializing chat for query: " + userQuery);
+
+            dynamic? json;
+
+            var queryObj = GetQueryObject(userQuery);
+            var stringObj = JsonSerializer.Serialize(queryObj);
+
+            Log("Query Json Object:");
             Log(stringObj);
-            var postData = new StringContent(stringObj, Encoding.UTF8, "application/json");
+
+            var postData = new StringContent(stringObj,
+                                                Encoding.UTF8,
+                                                "application/json");
 
             var modelResponse = PostAsync(postData);
+            Log("Model response received...");
+
             if (modelResponse.Result.Item1 == 200)
             {
-                Log("200 success response:");
+                Log("Success (200):");
                 Log(modelResponse.Result.Item2);
 
                 json = modelResponse.Result.Item2;
-                return Results.Content(json, "application/json", Encoding.UTF8, (int?)HttpStatusCode.OK);
+                return Results.Content(json,
+                                            "application/json",
+                                            Encoding.UTF8,
+                                            (int?)HttpStatusCode.OK);
+            }
+            else
+            {
+                Log($"Failed ({modelResponse.Result.Item1}):");
+
+                return Results.Content("{ 'Error': 'Phi3 - Request Failed (" + modelResponse.Result.Item2 + ")'}",
+                                            "application/json",
+                                            Encoding.UTF8,
+                                            modelResponse.Result.Item1);
             }
 
-            Log("failed response with status code:");
-            Log(modelResponse.Result.Item1.ToString());
-
-            json = "{ 'error': 'phi3 - request failed'}";
-            return Results.Content(json, "application/json", Encoding.UTF8, modelResponse.Result.Item1);
         }
-
         catch (Exception ex)
         {
-            Log("Error: " + ex.Message);
-
-            json = "{ 'error': 'phi3 - request failed'}";
-            return Results.Content(json, "application/json", Encoding.UTF8, (int?)HttpStatusCode.InternalServerError);
+            Log("Exception occured: " + ex.Message);
+            return Results.Content("{ 'Error': 'Phi3 - Request Failed With Exception (" + ex.Message + ")'}",
+                                        "application/json",
+                                        Encoding.UTF8,
+                                        (int?)HttpStatusCode.InternalServerError);
 
         }
     }
@@ -106,30 +127,39 @@ public class Phi3Chat
     {
         try
         {
-            var httpClient = new HttpClient();
-            httpClient.Timeout = new TimeSpan(0, AppConstants.OLLAMA_REQUEST_TIMEOUT_MINUTES, 0);
+            var httpClient = new HttpClient
+            {
+                Timeout = new TimeSpan(0, AppConstants.OLLAMA_REQUEST_TIMEOUT_MINUTES, 0)
+            };
 
+            string responseContent = string.Empty;
             using (var response = await httpClient.PostAsync(AppConstants.OLLAMA_API_URL, postData))
             {
-                string resp = string.Empty;
                 if (response.Content != null)
-                    resp = response.Content.ReadAsStringAsync().Result;
-                return ((int)response.StatusCode, resp);
+                    responseContent = response.Content.ReadAsStringAsync().Result;
+
+                return ((int)response.StatusCode, responseContent);
             }
         }
         catch (Exception e)
         {
-            Log(e.Message);
-            return (500, e.Message);
+            Log("Exception calling Ollama Rest Api:" + e.Message);
+            return ((int)HttpStatusCode.InternalServerError, e.Message);
         }
     }
 }
 
 public static class AppConstants
 {
+    //the chat url for ollama rest api
     public const string OLLAMA_API_URL = "http://localhost:11434/api/chat";
+
+    //the model used
     public const string OLLAMA_MODEL = "phi3:mini";
-    public const int OLLAMA_REQUEST_TIMEOUT_MINUTES = 3;
+
+    //the timeout in minutes for the ollama rest api request
+    public const int OLLAMA_REQUEST_TIMEOUT_MINUTES = 10;
+
     public const int OLLAMA_API_RESPONSE_MAX_TOKENS = 100;
     public const bool STREAM_RESPONSE = false;
 }
